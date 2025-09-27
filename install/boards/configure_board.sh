b910f0955477c254e0691f60d91228143bcdf844
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-09-27 20:38:27 +0100

    Pi Zero 2W: configure minimal version
    - Update configure_board.sh.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- install/boards/configure_board.sh
+++ install/boards/configure_board.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Detect and configure hardware for each supported plataform
-VERSION="${VERSION:-ubuntu-24.04}"
+VERSION="${VERSION:-pi-zero-2w}"
 GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-srmainwaring/BlueOS}
 REMOTE="${REMOTE:-https://raw.githubusercontent.com/${GITHUB_REPOSITORY}}"
 ROOT="$REMOTE/$VERSION"

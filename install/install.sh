e10077596b6ea7c52ba6581a79b63ca4e3fce05d
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-10-05 16:20:02 +0100

    Pi Zero 2W Bookworm: disable board config
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- install/install.sh
+++ install/install.sh
@@ -11,7 +11,7 @@ ROOT="$REMOTE/$VERSION"
 alias curl="curl --retry 6 --max-time 15 --retry-all-errors --retry-delay 20 --connect-timeout 60"
 
 # Additional options
-DO_BOARD_CONFIG=1 # default to do the board config
+DO_BOARD_CONFIG=0 # default to do the board config
 RUNNING_IN_CI=0 # default to not running CI mode/docker-in-docker
 
 usage_help()
